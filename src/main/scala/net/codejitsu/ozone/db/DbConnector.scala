3c03c877250f1ec1951f0c81defb516e3743870b
Alex <me@codejitsu.net>
2018-05-19 20:20:46 +0200

    Added table creation stuff.

--- src/main/scala/net/codejitsu/ozone/db/DbConnector.scala
+++ src/main/scala/net/codejitsu/ozone/db/DbConnector.scala
@@ -2,11 +2,17 @@ package net.codejitsu.ozone.db
 
 import java.sql.DriverManager
 
+import com.typesafe.scalalogging.LazyLogging
 import net.codejitsu.ozone.parser.ColumnInfo
 
 import scala.util.Try
 
-class DbConnector {
+class DbConnector extends LazyLogging {
+  def getType(name: String): String = name match {
+    case "number" => "INT"
+    case _ => "VARCHAR(255)"
+  }
+
   def createTable(columns: Seq[ColumnInfo], url: String, user: String, pwd: String): Try[Boolean] = Try {
     try {
       Class.forName("com.mysql.jdbc.Driver")
@@ -14,15 +20,16 @@ class DbConnector {
 
       val stmt = conn.createStatement()
 
+      val fields = columns.map(c => s"${c.fieldName.dropWhile(ch => ch == ':')} ${getType(c.dataTypeName)}")
+
       val sql =
-        """
-          |CREATE TABLE REGISTRATION
-          |(id INTEGER not NULL,
-          | first VARCHAR(255),
-          | last VARCHAR(255),
-          | age INTEGER,
-          | PRIMARY KEY ( id ))
+        s"""
+          |CREATE TABLE ozone_data
+          |(${fields.mkString(",")},
+          | PRIMARY KEY ( sid ))
         """.stripMargin
+
+      logger.info(sql)
 
       stmt.executeUpdate(sql)
     } catch {
